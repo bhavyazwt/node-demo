812fbc98f23c35ac9533ce1162003055b5205c20
bhavyazwt <bhavya@zealousweb.com>
2025-01-29 11:29:29 +0530

    added pool

--- exercises/day-5/db/migrationsRunner.js
+++ exercises/day-5/db/migrationsRunner.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const { pool } = require("../db/db");
 async function runMigrations() {
   try {
     console.log("Running migrations...");
